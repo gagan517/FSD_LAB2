cbff3a886684d9664a15d28534618162d847b417
Suryansh Shandilya <suryanshshandilya1712@gmail.com>
2026-02-23 11:50:09 +0530

    Experiment-5

---
+++ Experiment-5/src/user.jsx
@@ -0,0 +1,15 @@
+function User() {
+    return (
+      <div>
+        <h1>User name is Suryansh Shandilya</h1>
+        <h1>User name is Suryansh Shandilya</h1>
+        <h1>User name is Suryansh Shandilya</h1>
+        <h1>User name is Suryansh Shandilya</h1>
+        <h1>User name is Suryansh Shandilya</h1>
+        <h1>User name is Suryansh Shandilya</h1>
+        <h1>User name is Suryansh Shandilya</h1>
+      </div>
+    );
+  }
+  
+  export default User;
